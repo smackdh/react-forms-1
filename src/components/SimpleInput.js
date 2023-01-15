1ec2a4e736c98e21c441df28c176d021ef97ebef
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-15 09:35:10 +0900

    added useState, useRef

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,22 @@
+import React, { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const [nameInput, setNameInput] = useState("");
+
+  const inputHandler = (e) => {
+    setNameInput(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    inputHandler();
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" />
       </div>
       <div className="form-actions">
         <button>Submit</button>

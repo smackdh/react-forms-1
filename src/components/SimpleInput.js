9d1228d43a75b38270de2d5eb0e9e9ae7ba46108
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-15 14:20:03 +0900

    refactored code

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,48 +1,32 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
   const [nameInput, setNameInput] = useState("");
-  const [nameInputValid, setNameInputValid] = useState(false);
+  // const [nameInputValid, setNameInputValid] = useState(false);
   const [userTouchedInput, setUserTouchedInput] = useState(false);
-  const nameInputRef = useRef();
 
-  useEffect(() => {
-    if (nameInputValid) {
-      console.log("Name input is ok");
-    }
-  }, [nameInputValid]);
+  const enteredNameisValid = nameInput.trim() !== "";
+  const nameInputIsInvalid = !enteredNameisValid && userTouchedInput;
 
   const inputHandler = (e) => {
     setNameInput(e.target.value);
-
-    if (e.target.value.trim() !== "") {
-      setNameInputValid(true);
-      return;
-    }
   };
 
   const nameInputBlurHandler = (e) => {
     setUserTouchedInput(true);
-
-    if (nameInput.trim() === "") {
-      setNameInputValid(false);
-      return;
-    }
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
     setUserTouchedInput(true);
 
-    if (nameInput.trim() === "") {
-      setNameInputValid(false);
+    if (!enteredNameisValid) {
       return;
     }
 
-    setNameInputValid(true);
+    setNameInput("");
+    setUserTouchedInput(false);
   };
-
-  const nameInputIsInvalid = !nameInputValid && userTouchedInput;
 
   const nameInputClasses = nameInputIsInvalid
     ? "form-control invalid"
@@ -53,7 +37,6 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onBlur={nameInputBlurHandler}

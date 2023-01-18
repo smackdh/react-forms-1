acc3e943a2da278cd87c907d3c2ca3f326d23081
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-18 15:01:28 +0900

    added base handlers

---
+++ src/hooks/input-hook.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+const useInput = (validateValue) => {
+  const [inputValue, setInputValue] = useState("");
+  const [inputTouched, setInputTouched] = useState(false);
+
+  const isValid = validateValue(inputValue);
+  // check for errors?
+
+  const inputChangeHandler = (e) => {
+    setInputValue(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const inputTouchedHandler = (e) => {
+    setInputTouched(true);
+    console.log("TOUCHED!");
+  };
+
+  const reset = () => {
+    setInputValue("");
+    setInputTouched(false);
+    console.log("RESET");
+  };
+
+  return {
+    value: inputValue,
+    isInputValid: isValid,
+  };
+};
+
+export default useInput;
+
+// Add State
+// Create States
+// Check States
+// Add Handlers to take the value
+// Add onChange on the form
+// Add SubmitHandler
+// Add Reset handler to clear form
+// Add function to the components argument
+// Import the custom hook
+// Give names to the things
+// Add specific validation checks (regex etc)

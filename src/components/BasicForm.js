30afdb33f777c04c4a3f752d0f39ecd972012e8d
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-18 15:19:37 +0900

    added logic for classes

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,10 +1,11 @@
 import useInput from "../hooks/input-hook";
+import React, { useEffect } from 'react'
 
 const BasicForm = (props) => {
   const {
     value: nameInputValue,
     isInputValid: isNameValid,
-    hasError,
+    hasError: nameHasError,
     inputChangeHandler: nameChangeHandler,
     inputBlurHandler: nameBlurHandler,
     reset: nameReset,
@@ -12,16 +13,26 @@ const BasicForm = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
+
+    nameReset();
   };
+
+  const nameInputClasses = !isNameValid ? "form-control invalid" : "form-control "};
+
 
   return (
     <form onSubmit={submitHandler}>
       <div className="control-group">
-        <div className="form-control">
+        <div className={nameInputClasses}>
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" />
+          <input
+            onBlur={nameBlurHandler}
+            onChange={nameChangeHandler}
+            type="text"
+            id="name"
+          />
         </div>
-
+        {nameHasError ? <p>Enter a name.</p> : ""}
         <div className="form-control">
           <label htmlFor="name">Last Name</label>
           <input type="text" id="name" />
